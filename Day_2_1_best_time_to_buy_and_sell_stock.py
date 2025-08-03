7bdc07471b73440b94a8f7827f31c69d4887f9d2
Premchand Ancha <anchapremchand@gmail.com>
2025-08-03 22:55:29 +0530

    Create Day_2_1_best_time_to_buy_and_sell_stock.py

---
+++ Day_2_1_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,14 @@
+def maxProfit(prices):
+    min_price = float('inf')
+    max_profit = 0
+
+    for price in prices:
+        min_price = min(min_price, price)
+        profit = price - min_price
+        max_profit = max(max_profit, profit)
+
+    return max_profit
+
+# Example call
+prices = [7, 1, 5, 3, 6, 4]
+print("Max Profit:", maxProfit(prices))
